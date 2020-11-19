4b860f8939a410c7f3e0ffad621dbaedf0d0d3cb
urjitb <urjitbhosale@gmail.com>
2020-11-20 02:29:26 +0530

    cleanup 3

--- index.js
+++ index.js
@@ -261,7 +261,7 @@ app.get("/dashboard", auth, async (req, res) => {
         axios.get('https://geolocation-db.com/json/09ba3820-0f88-11eb-9ba6-e1dd7dece2b8/' + req.ip)
             .then(function (response) {
                 //console.log(response.data.country_code)
-                axios.get('https://mobverify.com/api/v1/?affiliateid=74530&country=' + response.data.country_code + '&device=' + getDeviceType(req.get('User-Agent')) + '&ctype=15&aff_sub5=' + user.affid, function (data) {
+                axios.get('https://mobverify.com/api/v1/?affiliateid=74530&country=' + 'US' + '&device=' + getDeviceType(req.get('User-Agent')) + '&ctype=15&aff_sub5=' + user.affid, function (data) {
                 }).then(function (response) {
 
                     let RTET = 0.0
@@ -289,6 +289,18 @@ app.get("/admin", adminAuth, async (req, res) => {
 
     } catch (e) {
 
+        res.send({ message: "Error in Fetching user" });
+
+    }
+});
+
+app.get("/faq", auth, async (req, res) => {
+    try {
+        
+        const user = await Users.findById(req.user.id);
+        res.render('faq',  { layout: 'loggedin.hbs', User: user.toObject() });
+
+    } catch (e) {
         res.send({ message: "Error in Fetching user" });
 
     }
